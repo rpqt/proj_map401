717f5629365aab96f593d3a6c3e07904998f1222
Romain Paquet <romain.pqt@gmail.com>
2023-01-24 14:25:39 +0100

    init

---
+++ TACHE1/test_image.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include "image.h"
+
+void test_ecrire_image()
+{
+	printf("\n");
+	printf("TEST ecrire_image\n");
+	printf("=================\n");
+
+	Image I = lire_fichier_image("caractere2.pbm");
+	printf("Image du caractère 2:\n");
+	ecrire_image(I);
+}
+
+void test_set_pixel_image()
+{
+	printf("\n");
+	printf("TEST set_pixel_image\n");
+	printf("=================\n");
+
+	Image I = creer_image(5, 5);
+	printf("Image vide 5x5:\n");
+	ecrire_image(I);
+
+	set_pixel_image(I, 2, 2, NOIR);
+	set_pixel_image(I, 4, 2, NOIR);
+	set_pixel_image(I, 2, 4, NOIR);
+	set_pixel_image(I, 3, 4, NOIR);
+	set_pixel_image(I, 4, 4, NOIR);
+
+	printf("Image visage 5x5:\n");
+	ecrire_image(I);
+}
+
+void test_negatif_image()
+{
+	printf("\n");
+	printf("TEST negatif_image\n");
+	printf("==================\n");
+
+	Image I = lire_fichier_image("caractere2.pbm");
+	printf("Image du caractère 2:\n");
+	ecrire_image(I);
+
+	Image I_neg = negatif_image(I);
+	printf("Négatif du caractère 2:\n");
+	ecrire_image(I_neg);
+}
+
+int main(int argc, char **argv)
+{
+	test_ecrire_image();
+	test_set_pixel_image();
+	test_negatif_image();
+	exit(0);
+}
