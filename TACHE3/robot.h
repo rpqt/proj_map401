c3a2e63ec4b06969cc7f8ca3ee7d8b5a27e674ac
Loïc Giraud <119013303+Neayd@users.noreply.github.com>
2023-01-31 15:47:39 +0100

    Create robot.h

---
+++ TACHE3/robot.h
@@ -0,0 +1,14 @@
+#include "../TACHE1/image.h"
+/* 
+ Type enuméré Orientation équivalent au char avec NORD=0,EST=1,SUD=2,OUEST=3
+ */
+typedef enum {NORD=0,EST=1,SUD=2,OUEST=3} Orientation;
+
+typedef struct Robot {
+    int x, y; /* Position */
+    Orientation o;
+} Robot;
+
+void avancer_robot(Robot *r);
+
+void orienter_robot(Robot *r, Image I);
