a09d47748a8bb15560d6e1797b2b6234351c283b
Romain Paquet <romain.pqt@gmail.com>
2023-02-07 17:24:08 +0100

    Ajoute les & où il faut

--- TACHE3/test_contour2.c
+++ TACHE3/test_contour2.c
@@ -9,20 +9,20 @@ int main()
     printf("C1 : "); affiche_contour(C1);
 
     /* ajouter les points (5,3),(3,1),(7,2) et (1,6) dans C1 */
-    C1 = ajouter_point(C1, set_point(5,3));
-    C1 = ajouter_point(C1, set_point(3,1));
-    C1 = ajouter_point(C1, set_point(7,2));
-    C1 = ajouter_point(C1, set_point(1,6));
+    C1 = ajouter_point(&C1, set_point(5,3));
+    C1 = ajouter_point(&C1, set_point(3,1));
+    C1 = ajouter_point(&C1, set_point(7,2));
+    C1 = ajouter_point(&C1, set_point(1,6));
     printf("C1 : "); affiche_contour(C1);
 
     /* ajouter le point (4,1) dans C1 */
-    C1 = ajouter_point(C1, set_point(4,1));
+    C1 = ajouter_point(&C1, set_point(4,1));
     printf("C1 : "); affiche_contour(C1);
 
     /* créer le contour C2 avec les points (9,5) et (5,7) */
     C2 = creer_sequence();
-    C2 = ajouter_point(C2, set_point(9,5));
-    C2 = ajouter_point(C2, set_point(5,7)); 
+    C2 = ajouter_point(&C2, set_point(9,5));
+    C2 = ajouter_point(&C2, set_point(5,7)); 
     printf("C2 : "); affiche_contour(C2);
 
     /* concaténer C2 à la suite de C1 */
