83da91a183a57da3d74eb22f225382d2648dcb9c
loic <girauloi@etu.univ-grenoble-alpes.fr>
2023-02-21 15:26:00 +0100

    commentaires et nouvelles fonctions sequence.h

--- TACHE5/sequence.h
+++ TACHE5/sequence.h
@@ -37,11 +37,39 @@ typedef struct Tableau_Point
     Point *tab; /* pointeur vers le tableau des éléments */
 } Tableau_Point;
 
+/* créer une CelluleContour de liste avec le point p 
+   renvoie le pointeur sur la CelluleContour créée 
+   la fonction s'arrete si la création n'a pas pu se faire */
 CelluleContour *creer_CelluleContour(Point p);
+
+/* créer une Cellule de liste avec le contour c
+   renvoie le pointeur sur la CelluleContour créée 
+   la fonction s'arrete si la création n'a pas pu se faire */
+Cellule *creer_Cellule(Contour c);
+
+/* créer un contour vide */
 Contour creer_Contour();
+
+/* créer une sequence vide */
+Sequence creer_Sequence();
+
+/* ajoute le point p en fin du contour c, renvoie le contour c modifiée */
 Contour ajouter_point(Contour *s, Point p);
+
+/* ajoute le contour c en fin de la sequence s, renvoie la sequence s modifiée */
+Sequence ajouter_contour(Sequence *s, Contour c);
+
+/* suppression de tous les éléments du contour, renvoie le contour c vide */
 Contour supprimer_Contour(Contour s);
+
+/* suppression de tous les éléments de la liste, renvoie la liste s vide */
+Sequence supprimer_sequence(Sequence s);
+
+/* concatène s2 à la suite de s1, renvoie la liste s1 modifiée */
 Contour concatener_Contours(Contour s1, Contour s2);
+
+/* créer une séquence de points sous forme d'un tableau de points
+   à partir de la liste de points s */
 Tableau_Point Contour_vers_tableau(Contour s);
 
 #endif
